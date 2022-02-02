721e4d9863532359de2e4e501c0c016614783eb9
carolctrindade <karoolcosta10@gmail.com>
2022-02-01 23:07:41 -0300

    Aulas
    Acompanhando as aulas

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65 
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60){ //condição simples
+    console.log(`Você ultrapassou a velocidade máxima permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = "EUA"
+console.log(`Vivendo em ${país}`)
+if (país != "Brasil"){
+    console.log("Estrangeiro(a)")
+}else{
+    console.log("Brasileiro(a)")
+}
